436be543ab06b8e3eee82677de549889e3488b9a
Administrator <Pa$$w0rd>
2017-07-19 09:39:52 +0000

    "commit"

--- src/test/scala/NumberSuite.scala
+++ src/test/scala/NumberSuite.scala
@@ -7,4 +7,21 @@ import org.scalatest.FunSuite
 class NumberSuite extends FunSuite
 {
 
+  test("Input a valid number.")
+  {
+    try(Main.printNum(12345))
+  }
+
+  test("Input an invalid, negative number.")
+  {
+    val error = intercept[Exception] {Main.printNum(-1)}
+    assert(error.getMessage == "For input string: \"-\"")
+  }
+
+  test("Input an invalid string instead of a number.")
+  {
+    val error = intercept[Exception] {Main.printNum("abc".toLong)}
+    assert(error.getMessage == "For input string: \"abc\"")
+  }
+
 }
